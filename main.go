1afeb56d787911a406e7c871a82a57e92c754a21
Kostiantyn Masliuk <1pkg@protonmail.com>
2021-04-11 10:09:21 +0200

    add structure

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package amb
+
+type AmbVar []interface{}
+
+func NewAmbVar(v ...interface{}) AmbVar {
+	return AmbVar(v)
+}
+
+type AmbFunc func(p ...interface{}) bool
+
+func Amb(f AmbFunc, vars ...AmbVar) AmbVar {
+	return nil
+}
